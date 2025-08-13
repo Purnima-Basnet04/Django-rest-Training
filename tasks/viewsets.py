37508d57288c95539564e1789068538bda282c0a
Purnima-Basnet04 <purnimabasnet101@gmail.com>
2025-08-13 20:53:40 +0545

    worked on viewsets

--- tasks/viewsets.py
+++ tasks/viewsets.py
@@ -2,8 +2,12 @@ from rest_framework import viewsets
 from .serializers import TaskSerializer
 from .models import Task
 from .permissions import*
+from django_filters import filters
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsTaskOwnerOrReadOnly]
+    filter_backends = [filters.orderingFilter]
+    ordering_fields = ['title','created_at','updated_at']
+    filterset_fields = ['status']
